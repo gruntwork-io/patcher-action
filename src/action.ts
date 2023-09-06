b0ae0ee166d25767645de57a08fc4194574d23e2
Marina Limeira <marina@gruntwork.io>
2023-09-06 16:10:46 +0200

    Remove upload artifact + move it all to a single file

--- src/action.ts
+++ src/action.ts
@@ -369,8 +369,6 @@ export async function run() {
   const workingDir = core.getInput("working_dir")
   const commitAuthor = core.getInput("commit_author")
 
-  console.log("Hi!")
-
   // Always mask the `token` string in the logs.
   core.setSecret(token)
 
