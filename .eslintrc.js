de2c1a7cde182479c81dc1f67d9df46a33eaaaa7
Rob Morgan <robbym@gmail.com>
2024-06-20 16:51:06 +0800

    update linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    root: true,
-    ignorePatterns: ["dist/index.js"],
-    rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-    }
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  ignorePatterns: ["dist/index.js"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+  },
 };
